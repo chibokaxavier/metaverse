040a23b3f025da1b7f922fab7b1b3907fcae7602
manlikexavy <chibokaxavier@gmail.com>
2024-12-07 14:38:50 +0100

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+     <Navbar />
+    <Hero />
+    <About />
+    <Explore />
+    <GetStarted />
+    <WhatsNew />
+    <World />
+    <Insights />
+    <Feedback />
+    <Footer />
+   </div>
+  );
+}
