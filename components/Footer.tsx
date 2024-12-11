2fea1e33de54579f127bd9b12d508a5be45a7c4f
manlikexavy <chibokaxavier@gmail.com>
2024-12-11 22:58:27 +0100

    done with footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
-
+"use client";
+import styles from "@/styles";
+import React, { useState } from "react";
+import {
+  fadeIn,
+  footerVariants,
+  staggerContainer,
+  zoomIn,
+} from "@/utils/motion";
+import { motion } from "framer-motion";
+import { TypingText, TitleText, StartSteps, InsightCard } from "@/components";
+import { insights, socials } from "@/constants";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <motion.footer
+      variants={footerVariants}
+      //@ts-ignore
+      className={`sm:p-16 xs:p-8 px-6 py-12  relative`}
+      initial="hidden"
+      whileInView="show"
+    >
+      <div className="footer-gradient" />
+      <div className="mx-auto 2xl:max-w-[1280px] w-full flex flex-col gap-8">
+        <div className="flex items-center justify-between flex-wrap gap-5 ">
+          <h4 className="font-bold md:text-[64px] text-[44px]  text-white ">
+            Enter the metaverse
+          </h4>{" "}
+          <button
+            type="button"
+            className="flex items-center h-fit py-4 px-6 bg-[#25618b] rounded-[32px] gap-[12px] "
+          >
+            <img
+              src="/headset.svg"
+              alt="headset"
+              className="size-[24px] object-contain"
+            />{" "}
+            <span className="font-normal text-[16px] text-white ">
+              ENTER METAVERSE
+            </span>
+          </button>
+        </div>
+        <div className="flex flex-col ">
+          <div className="mb-[50px] h-[2px] bg-white opacity-10 " />
+          <div className="flex items-center justify-between flex-wrap gap-4 ">
+            <h4 className="font-extrabold text-[24px] text-white ">
+              Metaversus
+            </h4>
+            <p className="font-normal text-[14px] text-white opacity-50 ">
+              {" "}
+              Copyright 2024-2025 Metaversus. All rights reserved{" "}
+            </p>
+            <div className="flex gap-4">
+              {socials.map((social, i) => (
+                <img
+                  key={i}
+                  src={social.url}
+                  alt={social.name}
+                  className="size-[24px] object-contain cursor-pointer"
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.footer>
+  );
+};
 
-export default Footer
+export default Footer;
